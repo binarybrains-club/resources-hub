ddbda449d51ecb6618a473b3f193cd9766d146cf
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2026-06-09 22:58:31 -0600

    feat: Installed neobrutalUI and added button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import preact from '@astrojs/preact';
-
 import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [react()],
   site: 'https://binarybrains-club.github.io',
   base: '/resources-hub',
 
@@ -15,3 +15,4 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 });
+
